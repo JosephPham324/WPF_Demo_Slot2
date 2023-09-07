1796318ce32e8529264e9b773fe76e335657fff1
JosephPham324 <quangpnce170036@fpt.edu.vn>
2023-09-07 07:43:51 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo_Slot2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnToPage01_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new PageDemo();
+        }
+        private void btnToPage02_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new PageDemo2();
+        }
+        private void btnToPage03_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new PageDemo3();
+        }
+        private void btnToPage04_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new PageDemo4();
+        }
+        private void btnToPage05_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new PageDemo5();
+        }
+    }
+}
---
+++ PageDemo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo_Slot2
+{
+    /// <summary>
+    /// Interaction logic for PageDemo.xaml
+    /// </summary>
+    public partial class PageDemo : Page
+    {
+        public PageDemo()
+        {
+            InitializeComponent();
+        }
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            string CarInfo = $"Car Name:{txtCarName.Text}\n" + $"Colour: {txtColor.Text}\nBrand: {txtBrand.Text}";
+            MessageBox.Show(CarInfo, "Car Details");
+        }
+    }
+}
